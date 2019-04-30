44ddfdf30967c85f9cf5e8a71463947ec1514af9
Tou Yang <Tou.Yang@renaissance.com>
2019-04-30 09:42:32 -0500

    update to combine dataset from provider and routes

--- AngularTutorialApp/src/app/current-routes/current-routes.component.ts
+++ AngularTutorialApp/src/app/current-routes/current-routes.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { TransitService } from '../transit-service/transit.service';
 import { ProviderOperation } from '../models/provider-operation';
 import { Observable, combineLatest } from 'rxjs';
+import { containsTree } from '@angular/router/src/url_tree';
 
 @Component({
   selector: 'app-current-routes',
@@ -11,10 +12,12 @@ import { Observable, combineLatest } from 'rxjs';
 export class CurrentRoutesComponent implements OnInit {
 
 
-  public routesObservable$: Observable<ProviderOperation[]>;
-  public providers$: Observable<any>;
-  public vehicleDirection$: Observable<any>;
-  public vehicleLocation$: Observable<any>;
+  private routesObservable$: Observable<ProviderOperation[]>;
+  private providers$: Observable<any>;
+  private vehicleDirection$: Observable<any>;
+  private vehicleLocation$: Observable<any>;
+  public displayVM: any;
+
 
   constructor(private tService: TransitService) { }
 
@@ -23,11 +26,38 @@ export class CurrentRoutesComponent implements OnInit {
     this.providers$ = this.tService.GetProviders();
 
     combineLatest(this.routesObservable$, this.providers$).subscribe(result => {
-      let routes = result[0];
-      let providers = result[1];
+      const routes = result[0];
+      const providers = result[1];
+
+      const combinedRoutesAndProvider = routes.map(route => {
+        const providerId = route.ProviderID;
+
+        const provider = providers.find(x => x.Value === providerId);
+
+        return {
+          ProviderName: provider.Text,
+          ProviderId: provider.Value,
+          RouteDescription: route.Description,
+          RouteId: route.Route
+        };
+      });
+
+      this.displayVM = combinedRoutesAndProvider.sort((current, next) => {
+        if (current.ProviderName > next.ProviderName) {
+          return 1;
+        } else if (current.ProviderName < next.ProviderName) {
+          return -1;
+        } else if (current.ProviderName === next.ProviderName) {
+          if (Number.parseInt(current.RouteId, 10) > Number.parseInt(next.RouteId, 10)) {
+            return 1;
+          } else if (Number.parseInt(current.RouteId, 10) < Number.parseInt(next.RouteId, 10)) {
+            return -1;
+          } else if (Number.parseInt(current.RouteId, 10) === Number.parseInt(next.RouteId, 10)) {
+            return 0;
+          }
+        }
+      });
     });
-
-
 
   }
 }
