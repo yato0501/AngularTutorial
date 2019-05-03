28776ec21e160ad1ec48ebdd5d3b7bc870cdba15
Tou Yang <yato0501@gmail.com>
2019-05-03 08:53:18 -0500

    remove unused code

--- AngularTutorialApp/src/app/app.component.spec.ts
+++ AngularTutorialApp/src/app/app.component.spec.ts
@@ -22,16 +22,4 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  // it(`should have as title 'AngularTutorialApp'`, () => {
-  //   const fixture = TestBed.createComponent(AppComponent);
-  //   const app = fixture.debugElement.componentInstance;
-  //   expect(app.title).toEqual('AngularTutorialApp');
-  // });
-
-  // it('should render title in a h1 tag', () => {
-  //   const fixture = TestBed.createComponent(AppComponent);
-  //   fixture.detectChanges();
-  //   const compiled = fixture.debugElement.nativeElement;
-  //   expect(compiled.querySelector('h1').textContent).toContain('Welcome to AngularTutorialApp!');
-  // });
 });
